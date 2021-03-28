ebd9b1afebf42226811c3f51be98baaadcee4dea
Victor Kist <victoraugustokist@gmail.com>
2021-03-28 18:58:14 -0300

    Update BuscaBinaria_Recursiva and BuscaBinaria_VetorOrdenado

--- pesquisa.py
+++ pesquisa.py
@@ -47,69 +47,47 @@ class VetorNaoOrdenado:  # classe para trabalhar com vetores estáticos (sem as 
 
         return -1  # se chegou até aqui não encontrou
 
-    def BuscaBinaria_VetorOrdenado(self, valor):
-        meio = int((len(self.valores) / 2))
-        print(f'meio == {meio}')
-        if valor == self.valores[meio]:
-            return meio
-        if valor < self.valores[meio]:
-            for i in range(meio):
-                if valor == self.valores[meio - i]:
-                    return meio - i
-        if valor > self.valores[meio]:
-            for i in range(meio+1):
-                if valor == self.valores[meio + i]:
-                    return meio + i
+    def BuscaBinaria_VetorOrdenado(self, numero):
+        inicio = 0
+        fim = self.capacidade - 1
+        meio = (inicio + fim) // 2
+        while numero != self.valores[meio] and inicio <= fim:
+            meio = (inicio + fim) // 2
+            if numero == self.valores[meio]:
+                return meio
+            elif numero < self.valores[meio]:
+                fim = meio - 1
+            elif numero > self.valores[meio]:
+                inicio = meio + 1
+        return -1
 
-    def BuscaBinaria_Recursiva(self, valor, inicio, fim):
-        meio = int(fim / 2) + inicio
-        print(f'meiorec == {meio}')
-        if valor == self.valores[meio]:
+    def BuscaBinaria_Recursiva(self, numero, inicio, fim):
+        meio = (inicio + fim) // 2
+        if numero == self.valores[meio]:
             return meio
-        if valor < self.valores[meio]:
-            self.BuscaBinaria_Recursiva(valor, 0, meio)
-        if valor > self.valores[meio]:
-            self.BuscaBinaria_Recursiva(valor, meio, len(self.valores))
+        elif inicio == fim:
+            return -1
+        elif numero < self.valores[meio]:
+            fim = meio - 1
+        elif numero > self.valores[meio]:
+            inicio = meio + 1
+        return self.BuscaBinaria_Recursiva(numero, inicio, fim)
 
     def Hashing(self, valor):
         return (valor % 31) + 1
 
 
 
-x = VetorNaoOrdenado(11)
-#for i in range(9000000):
-    #x.inserir(random.randrange(0, 555))  #inserindo valores aleatórios
 
-x.inserir(3)
-x.inserir(4)
-x.inserir(11)
-x.inserir(12)
-x.inserir(21)
-x.inserir(34)
-x.inserir(65)
-x.inserir(77)
-x.inserir(78)
-x.inserir(89)
-x.inserir(112)
 
-#poshas = x.Hashing(112)
-#print(f'poshas == {poshas}')
-posrec = x.BuscaBinaria_Recursiva(78, 0, len(x.valores))
-print(f'posrec == {posrec}')
-#pos = x.BuscaBinaria_VetorOrdenado(4)
-#print(f'pos == {pos}')
 
-#x.inserir(556)
-# x.ordena_bolha()
-# x.imprimir()
+x = VetorNaoOrdenado(50)
+for i in range(49):
+  x.inserir(random.randrange(0, 2000)) #inserindo valores aleatórios
 
-#antes = time.time()  # timestamp de antes de chamar a busca
-#pos = x.Busca_VetorNaoOrdenado(556)
-
-#segundos = time.time() - antes
-#print(f'Levou {segundos} segundos')
-
-#if pos >= 0:
-#    print(f'Valor encontrado na posição {pos}')
-#else:
-#    print('Valor não encontrado')
+x.inserir(575)
+x.ordena_bolha()
+x.imprimir()
+# posicao = x.BuscaBinaria_Recursiva(575, 0, 50)
+posicao = x.BuscaBinaria_VetorOrdenado(575)
+print(f'Valor encontrado na posição {posicao}')
